b61f0ddf0c8e24c08a2e93b921298669c728f054
RockandRolls <livelydev010@gmail.com>
2023-10-26 17:36:57 +0200

    changes in framework

--- db/pgClient.js
+++ db/pgClient.js
@@ -1,7 +1,6 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  connectionString: process.env.CONN_STR,
-});
+const pool = new Pool();
+  // new Pool({connectionString: process.env.CONN_STR,});
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// require('dotenv').config(); u can write it here or in package.json (scripts),
+//  to add env file functionality. normally vite does it automatically in frontend
 const express = require("express");
 const app = express();
 const dbPool = require("./db/pgClient");
@@ -6,6 +8,9 @@ const cors = require("cors");
 
 const port = process.env.PORT || 7000;
 app.use(cors());
+
+console.log(process.env);
+// to see in terminal all the credentials added in env file
 
 app.get("/", (req, res) => res.send("Welcome to the films API"));
 
