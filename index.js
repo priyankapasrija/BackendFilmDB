e81cc93d095d21c1962c460194492bc0dec3933b
Catia <wolfgangcatia@gmail.com>
2023-10-26 19:39:57 +0200

    added initial search logic

--- index.js
+++ index.js
@@ -5,11 +5,30 @@ const { getAllFilms, getSingleFilm } = require("./controllers/filmControllers");
 const cors = require("cors");
 
 const port = process.env.PORT || 7000;
-app.use(cors());
+
+const corsOptions = {
+    origin: 'http://localhost:7000', // Reemplaza esto con la URL de tu frontend local
+    optionsSuccessStatus: 200 // Algunos navegadores requieren esta opción
+};
+
+
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => res.send("Welcome to the films API"));
 
 app.route("/films").get(getAllFilms);
 app.route("/films/:id").get(getSingleFilm);
+
+app.get('/search', async (req, res) => {
+    try {
+    const { query } = req.query 
+    const { rows } = await pool.query(
+        'SELECT * FROM films WHERE title=%$1% OR description=%$1% OR director=%$1% OR release_year=%$1%',
+        [query])
+    return res.json(rows)
+    } catch (error) {
+    return res.status(500).json({ error: error.message })
+    }
+});
 
 app.listen(port, () => console.log("Welcome"));
